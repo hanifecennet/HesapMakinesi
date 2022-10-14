bbfb0ed6a2af342ac97d7c3a58bbd66e2b7d6846
Hanife Cennet Alkan <hanifecennetalkan@gmail.com>
2022-10-14 14:42:35 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HesapMakinesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Toplama İŞlemei
+            
+            Console.WriteLine("Toplama işleminin sonucu: " + Toplama(3,5));//çağrıldığı yer
+        }
+
+        //Toplama işlemi
+
+        static  int Toplama(int sayi1, int sayi2) //metot tanımlandı
+        {
+            return sayi1 + sayi2;
+        }
+    }
+}
